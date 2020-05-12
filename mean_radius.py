1123a6d10ae0a430a1de377c1cb4a365dc208f23
rotemmayo <rotem.mayo@gmail.com>
2020-05-12 15:55:38 +0300

    Add what I have so far: 1. Code to get a video, break it into frames, find the edge of each frame. 2. Option to make a new video with the edge frames 3. Calculate and plot the mean radius of each frame in the video

---
+++ mean_radius.py
@@ -0,0 +1,142 @@
+"""
+Code for processing fractal viscous fingering images
+by extracting radius and angle information from binarized images.
+
+Stephen Morris August 2017
+
+How to use this code for processing binarized FVF (just outlines: not floodfilled) images:
+
+Images are assumed to have been be binarized using
+ empty_pipe = True
+ edge_pipe = False
+in the process_FVF_images.py code.
+
+All binarized images are stored in the image folder named in the code below.  They should
+look like just black outlines of the bubble, with a white gap and no inlet pipe etc.
+
+This code finds the mean radius and some statistics versus the frame number.
+You must supply the x,y coodinates of the centre of the inlet pipe fitting.
+These values should match the ones previously used in the process_FVF_images.py code
+
+You can uncomment a quick plot to check for outliers below.
+
+Results are stored in a text file called image_folder_name_radius_data.txt
+for further analysis.  At this stage, the units are still pixels and frame numbers.
+
+"""
+
+import os
+import os.path as path
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# --- Parameters  --- {{{
+from PIL import Image
+from tqdm import tqdm
+
+INPUT_DIR = './data/edge_frames'
+
+#  The x, y coordinates of the pixel located in the center of the inlet fitting.
+#  these depend on the precise location of the camera
+x_center, y_center = 192, 340
+
+# -------------------- }}}
+
+
+# index to number the frames starting from 0
+iframe = 0
+
+# pick out only tifs and not hidden files.
+# Files are sorted by frame number as long as only one shot of the camera is in the directory.
+inext = '.png'
+file_list = os.listdir(INPUT_DIR)
+file_list = sorted([name for name in file_list if (name.endswith(inext) and not name.startswith('.'))])
+
+fplot = []  # frame numbers for plotting
+rplot = []  # radii
+stdplot = []  # std dev
+rmaxplot =[]  # max radius
+rminplot =[]  # min radius
+
+
+for image_file_name in tqdm(file_list):
+    img = Image.open(path.join(INPUT_DIR, image_file_name))
+
+    # the transpose makes the coordinates of images match those of matrices
+    img_array = np.array(img).transpose()
+
+    img.close()
+
+    # make empty lists to hold the pixel radius data
+    radius = []
+    angle = []
+
+    for x in range(0, img_array.shape[0]):
+        for y in range(0, img_array.shape[1]):
+            if img_array[x, y] == 255 and (x-x_center) < 0:
+                x_pos = float(x-x_center)
+                y_pos = float(y-y_center)
+                r = np.sqrt(x_pos**2 + y_pos**2)
+
+                radius.append(r)
+                if r != 0.0:   # avoid zero radii, just in case
+                    if x_pos != 0.0:  # avoid a divide by zero situation
+                        angle.append(np.arctan(y_pos / x_pos))   # in radians
+                    else:
+                        if y_pos > 0.0:
+                            angle.append(np.pi/2.0)
+                        else:
+                            angle.append(-np.pi/2.0)
+                else:  # angle is undefined for zero radius: just make it zero
+                    angle = 0.0   # this should never happen
+
+    # end of the loop over 1 frame
+
+    # # uses an interquartile radius test to eliminate outliers due to stray black pixels
+    # radius, angle = fvf.kill_outliers(radius, angle)
+
+    fplot = fplot + [iframe]
+    rplot = rplot + [np.mean(radius)]
+    # standard deviation is a measure of how noncircular the bubble is.
+    stdplot = stdplot + [np.std(radius)]
+
+    rmaxplot = rmaxplot + [max(radius)]
+    rminplot = rminplot + [min(radius)]
+
+    # Use a quick plot to check for outliers due to wild stray pixels
+    # these should have been eliminated by kill_outliers function
+    # plt.plot(angle,radius,'.')
+    # plt.show()
+
+    # go to the next frame
+    iframe += 1
+
+# save the data to a file for further analysis later
+OUTPUT_DIR = './data/radius_results/'
+
+np.savetxt(OUTPUT_DIR + '_radius_data.txt', (fplot,rplot,stdplot,rmaxplot,rminplot))
+
+plt.plot(fplot, rplot, label='avg radius')
+plt.plot(fplot, stdplot, label='std dev')
+plt.plot(fplot, rmaxplot, label=r'$R_{max}$')
+plt.plot(fplot, rminplot, label= r'$R_{min}$')
+plt.xlabel('frame number')
+plt.ylabel('radius (in pixels)')
+plt.title('Radii ' + OUTPUT_DIR)
+plt.legend(loc='upper left')
+
+plt.savefig(OUTPUT_DIR+'radii.pdf')
+plt.show()
+
+plt.close()
+
+plt.plot(fplot, rplot, label='avg radius')
+plt.xlabel('frame number')
+plt.ylabel('radius (in pixels)')
+plt.title('Radii ' + OUTPUT_DIR)
+plt.legend(loc='upper left')
+
+plt.savefig(OUTPUT_DIR+'radii_only.pdf')
+plt.show()
+
---
+++ process_video.py
@@ -0,0 +1,126 @@
+import os
+import os.path as path
+import shutil
+import warnings
+
+import skimage.io as io
+from PIL import Image, ImageEnhance, ImageFilter
+import numpy as np
+from tqdm import tqdm
+
+
+def _load_as_greyscale(filename, contrast_factor=1.0, sharpness_factor=1.0, brightness_factor=1.0):
+    """
+    :return greyscale (0-255)  numpy array
+    """
+    img = Image.open(filename)
+
+    contrast = ImageEnhance.Contrast(img)
+    img = contrast.enhance(contrast_factor)
+
+    sharpness = ImageEnhance.Sharpness(img)
+    img = sharpness.enhance(sharpness_factor)
+
+    brightness = ImageEnhance.Brightness(img)
+    img = brightness.enhance(brightness_factor)
+
+    img_greyscale = img.convert('L')
+
+    # the transpose makes the coordinates of images match those of matrices
+    img_array = np.array(img_greyscale).transpose()
+    img.close()
+    img_greyscale.close()
+    return img_array
+
+
+def _binarize(image, threshold=128):
+    """
+    :param image: 2D array
+    :param threshold: int
+    :return: array[bool]
+    """
+    np_img = np.array(image)
+    binary_img = np_img > threshold
+    return binary_img
+
+
+def _radial_mask(img, x_center, y_center, inner, outer):
+    w, h = img.shape
+    x, y = np.mgrid[:w, :h]
+    radius = (x - x_center) ** 2 + (y - y_center) ** 2
+    return np.where((radius < inner ** 2) | (radius > outer ** 2), 255, img)
+
+
+def convert_to_bw():
+    INPUT_DIR = './data/input_video_frames'
+    OUTPUT_DIR = './data/bw_frames'
+
+    shutil.rmtree(OUTPUT_DIR)
+    os.mkdir(OUTPUT_DIR)
+
+    #  The x, y coordinates of the pixel located in the center
+    x_center, y_center = 192, 340
+
+    R_MIN = 0.
+    R_MAX = 147.
+
+    CONTRAST = 1.0  # makes the grey scale gradient steeper to better define the edge of the bubble, 1.0 => no change
+    SHARPNESS = 1.0  # emphasizes sharp edges of the bubble, 1.0 => no change
+    BRIGHTNESS = 1.0  # lifts the white level to whiter, 1.0 => no change
+
+    BW_THRESHOLD = 100  # A number between 0 (black) - 255 (white).
+
+    input_extension = '.png'
+    file_list = os.listdir(INPUT_DIR)
+    file_list = sorted([name for name in file_list if (name.endswith(input_extension) and not name.startswith('.'))])
+
+    output_extension = '.png'
+
+    for image_file_name in tqdm(file_list):
+        img = _load_as_greyscale(path.join(INPUT_DIR, image_file_name), CONTRAST, SHARPNESS, BRIGHTNESS)
+        img = _radial_mask(img, x_center, y_center, R_MIN, R_MAX)
+        img = _binarize(img, BW_THRESHOLD) * 255
+
+        image_file_name = image_file_name.replace(input_extension, '')
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            io.imsave(path.join(OUTPUT_DIR, image_file_name + output_extension), img, check_contrast=False)
+
+
+def find_edge():
+    INPUT_DIR = './data/bw_frames'
+    OUTPUT_DIR = './data/edge_frames'
+
+    shutil.rmtree(OUTPUT_DIR)
+    os.mkdir(OUTPUT_DIR)
+
+    input_extension = '.png'
+    file_list = os.listdir(INPUT_DIR)
+    file_list = sorted([name for name in file_list if (name.endswith(input_extension) and not name.startswith('.'))])
+
+    output_extension = '.png'
+
+    for image_file_name in tqdm(file_list):
+        img = Image.open(path.join(INPUT_DIR, image_file_name))
+        img = img.filter(ImageFilter.FIND_EDGES)
+
+        image_file_name = image_file_name.replace(input_extension, '')
+        img.save(path.join(OUTPUT_DIR, image_file_name + output_extension), 'png')
+
+
+def make_movie():
+    INPUT_DIR = './data/edge_frames'
+    OUTPUT_DIR = './data/output_video/'
+
+    shutil.rmtree(OUTPUT_DIR)
+    os.mkdir(OUTPUT_DIR)
+    os.system("ffmpeg -f image2 -r 20 -i " + INPUT_DIR + "output-%07d.png -vcodec mpeg4 -y "+OUTPUT_DIR+"edge_video.mp4")
+
+
+def frames_from_movie(video_name):
+    INPUT_DIR = './data/input_video/'
+    OUTPUT_DIR = './data/input_video_frames/'
+
+    shutil.rmtree(OUTPUT_DIR)
+    os.mkdir(OUTPUT_DIR)
+    os.system('ffmpeg -i ' + INPUT_DIR + video_name + '.mp4 ' + OUTPUT_DIR + 'output-%07d.png')
