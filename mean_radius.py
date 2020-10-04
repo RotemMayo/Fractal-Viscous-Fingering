647a89f634767b3892291afe437a5a32140bc3aa
rotemmayo <rotem.mayo@gmail.com>
2020-10-04 12:46:15 +0300

    Remove old docs

--- mean_radius.py
+++ mean_radius.py
@@ -1,29 +1,4 @@
-"""
-Code for processing fractal viscous fingering images
-by extracting radius and angle information from binarized images.
 
-Stephen Morris August 2017
-
-How to use this code for processing binarized FVF (just outlines: not floodfilled) images:
-
-Images are assumed to have been be binarized using
- empty_pipe = True
- edge_pipe = False
-in the process_FVF_images.py code.
-
-All binarized images are stored in the image folder named in the code below.  They should
-look like just black outlines of the bubble, with a white gap and no inlet pipe etc.
-
-This code finds the mean radius and some statistics versus the frame number.
-You must supply the x,y coodinates of the centre of the inlet pipe fitting.
-These values should match the ones previously used in the process_FVF_images.py code
-
-You can uncomment a quick plot to check for outliers below.
-
-Results are stored in a text file called image_folder_name_radius_data.txt
-for further analysis.  At this stage, the units are still pixels and frame numbers.
-
-"""
 
 import os
 import os.path as path
